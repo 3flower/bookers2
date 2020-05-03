3594f733ffab464ed0785c26560b67890271767e
atu <61017515+3flower@users.noreply.github.com>
2020-05-03 19:58:20 +0900

    コメント機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ApplicationRecord
 	#presence trueは空欄の場合を意味する。
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
+	has_many :book_comments, dependent: :destroy
 end
